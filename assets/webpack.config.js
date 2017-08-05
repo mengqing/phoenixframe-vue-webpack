34856573a1ad9cb898eca615b25da5819b38cb70
Jimmy Zhang <mengqing@gmail.com>
2017-08-05 20:36:07 +1000

    added loaders

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,25 +1,23 @@
 /**
  * Modules
  */
-const path = require('path');
-const webpack = require('webpack');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
+const autoprefixer = require("autoprefixer");
 
 /**
  * Configuration
  */
 module.exports = (env) => {
-  const isDev = !(env && env.prod);
-  const devtool = isDev ? "eval" : "source-map";
-
   return {
-    devtool: devtool,
-
     context: __dirname,
 
     entry: {
       app: [
-        "js/app.js"
+        "js/app.js",
+        "css/app.css"
       ]
     },
 
@@ -37,20 +35,69 @@ module.exports = (env) => {
           query: {
             presets: ["env"]
           }
+        },
+
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          exclude: /node_modules/,
+          loaders: [
+            "file-loader?name=images/[name].[ext]",
+            {
+              loader: "image-webpack-loader",
+              options: {
+                query: {
+                  mozjpeg: {
+                    progressive: true
+                  },
+                  gifsicle: {
+                    interlaced: true
+                  },
+                  optipng: {
+                    optimizationLevel: 7
+                  },
+                  pngquant: {
+                    quality: "65-90",
+                    speed: 4
+                  }
+                }
+              }
+            }
+          ]
+        },
+
+        {
+          test: /\.(ttf|woff2?|eot|svg)$/,
+          exclude: /node_modules/,
+          query: { name: "fonts/[hash].[ext]" },
+          loader: "file-loader"
+        },
+
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: ExtractTextWebpackPlugin.extract({
+            fallback: "style-loader",
+            use: ["css-loader", "postcss-loader"]
+          })
         }
       ]
     },
 
     resolve: {
       modules: ["node_modules", __dirname],
-      extensions: [".js"]
+      extensions: [".js", ".css"]
     },
 
     plugins: [
       new CopyWebpackPlugin([{
         from: "./static",
         to: path.resolve(__dirname, "../priv/static")
-      }])
+      }]),
+
+      new ExtractTextWebpackPlugin({
+        filename: "css/[name].css",
+        allChunks: true
+      })
     ]
   }
 }
